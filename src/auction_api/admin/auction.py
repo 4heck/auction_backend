f987a6feabeb3fbb45bd593e16ef9df9a38c63c1
4heck <mamleev.rus@gmail.com>
2020-09-29 20:08:52 +0300

    - has_delete_permission

--- src/auction_api/admin/auction.py
+++ src/auction_api/admin/auction.py
@@ -4,9 +4,6 @@ from django.contrib import admin
 class AuctionModelAdmin(admin.ModelAdmin):
     list_display = ("title", "get_is_active")
 
-    def has_delete_permission(self, request, obj=None):
-        return False
-
     def get_is_active(self, obj):
         return obj.is_active
 
