22884e389e9b409beb42934ae07ae2a3485f0e7a
4heck <mamleev.rus@gmail.com>
2020-10-08 18:59:17 +0300

    + get_bids

--- src/auction_api/serializers/auction.py
+++ src/auction_api/serializers/auction.py
@@ -1,12 +1,19 @@
 from rest_framework import serializers
 
-from auction_api.models import Auction
+from auction_api.models import Auction, Bid
+from auction_api.serializers.bid import BidSerializer
 
 
 class AuctionSerializer(serializers.ModelSerializer):
+    bids = serializers.SerializerMethodField()
+
     class Meta:
         model = Auction
         fields = "__all__"
 
     def create(self, validated_data):
         return super(AuctionSerializer, self).create(validated_data)
+
+    def get_bids(self, auction: Auction):
+        bids = Bid.objects.filter(auction=auction)
+        return BidSerializer(instance=bids, many=True).data
